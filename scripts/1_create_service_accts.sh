4d381b351209f713c5fb0b18165e04f5a7bd4b11
Alex Smith <alex@Alexs-MacBook-Air.local>
2025-08-04 15:46:54 +1000

    Another attempt to fix service account permissions

--- scripts/1_create_service_accts.sh
+++ scripts/1_create_service_accts.sh
@@ -11,7 +11,11 @@ gcloud iam service-accounts create $SVC_ACCT \
 gcloud iam service-accounts keys create ~/.gcp/${SVC_ACCT}-key.json \
     --iam-account=$SVC_EMAIL
 
+# impersonation
 gcloud iam service-accounts add-iam-policy-binding $SCRAPER_SVC_EMAIL \
+  --member="serviceAccount:$SVC_EMAIL" \
+  --role="roles/iam.serviceAccountUser"
+gcloud iam service-accounts add-iam-policy-binding $SVC_EMAIL \
   --member="serviceAccount:$SVC_EMAIL" \
   --role="roles/iam.serviceAccountUser"
 
