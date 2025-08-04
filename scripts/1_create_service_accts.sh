2b0d9b331e11bab4b2a951234f8221929ea863d0
Alex Smith <alex@Alexs-MacBook-Air.local>
2025-08-04 15:31:26 +1000

    Another attempt to fix service account permissions

--- scripts/1_create_service_accts.sh
+++ scripts/1_create_service_accts.sh
@@ -1,6 +1,8 @@
 source ../gcp.env
 
+######################################
 ### Service account for deployments
+######################################
 
 gcloud iam service-accounts create $SVC_ACCT \
     --description="Service account for deployments and scheduling" \
@@ -13,15 +15,24 @@ gcloud iam service-accounts add-iam-policy-binding $SCRAPER_SVC_EMAIL \
   --member="serviceAccount:$SVC_EMAIL" \
   --role="roles/iam.serviceAccountUser"
 
+# artifact registry
 gcloud projects add-iam-policy-binding $PROJECT \
     --member=serviceAccount:$SVC_EMAIL \
     --role=roles/artifactregistry.writer
 
+# cloud run jobs
 gcloud projects add-iam-policy-binding $PROJECT \
     --member=serviceAccount:$SVC_EMAIL \
     --role=roles/run.admin
 
+# cloud scheduler
+gcloud projects add-iam-policy-binding $PROJECT \
+    --member=serviceAccount:$SVC_EMAIL \
+    --role=roles/cloudscheduler.admin
+
+######################################
 ### Service account for scraper jobs
+######################################
 
 gcloud iam service-accounts create $SCRAPER_SVC_ACCT \
     --description="Service account for NRL data ingestion" \
