fccc778bf45ce04a0c933378af6b2a7a3cc13135
Alex Smith <alex@Alexs-MacBook-Air.local>
2025-08-06 17:32:19 +1000

    Basic feature engineering and model training added

--- scripts/3_create_artifact_repo.sh
+++ scripts/3_create_artifact_repo.sh
@@ -3,8 +3,10 @@
 
 source ../gcp.env
 
-gcloud artifacts repositories create $DOCKER_REPO --repository-format=docker \
-    --location=$REGION --description="Docker repository for NRL predictor" \
+gcloud artifacts repositories create $DOCKER_REPO \
+    --repository-format=docker \
+    --location=$REGION \
+    --description="Docker repository for NRL predictor" \
     --project=$PROJECT
 
 # configure Docker to use the Google Cloud CLI to authenticate requests to Artifact Registry.
